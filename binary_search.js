5b4cf195d8054deb444f649bd2c1fd957f0cd932
ju-kkim <wndus8198@naver.com>
2021-12-15 22:26:19 +0900

    feat : targetNum이 배열 중간값보다 작을 때 binarySearch 함수 리턴
    - 재귀함수 사용

--- binary_search.js
+++ binary_search.js
@@ -4,6 +4,13 @@ function binarySearch(array, targetNum) {
         console.log('찾았다', array[middle]);
         return
     }
+
+    if(array[middle] > targetNum){
+        const leftNum = array.slice(0,middle);
+        console.log('작아', array[middle])
+        console.log('leftNum', leftNum)
+        return binarySearch(leftNum, targetNum)
+    }
 }
 let numArray = [1,3,6,20,25,100];
-binarySearch(numArray, 20);
+binarySearch(numArray, 1);
